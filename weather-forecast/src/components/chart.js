636f58f2f1f30206a3adc03513246f164a6f0e46
Jingxuan Hu <jinghu@visa.com>
2017-01-10 15:06:26 -0800

    finish weather_forecast

---
+++ weather-forecast/src/components/chart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
+
+export default function Chart(props) {
+  return (
+    <td><Sparklines height={120} width={180} data={props.data}>
+      <SparklinesLine color={props.color} />
+      <SparklinesReferenceLine type="avg" />
+      <div>{average(props.data)} {props.unit}</div>
+    </Sparklines></td>
+  );
+}
